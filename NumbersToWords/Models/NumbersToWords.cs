6bfc4fd60c79f5ba0fbf8f61200ef8576869f656
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2021-12-09 15:42:51 -0900

    add commented lofic for thousands

--- NumbersToWords/Models/NumbersToWords.cs
+++ NumbersToWords/Models/NumbersToWords.cs
@@ -18,24 +18,28 @@ namespace NumbersToWords.Models
     {
       {2, "twenty"},{3, "thirty"},{4, "forty"},{5, "fifty"},{6, "sixty"},{7, "seventy"},{8, "eighty"},{9, "ninety"}
     };
-        public static string ChangeToWords(string input)
+    public static Dictionary<int, string> sub1000 = new Dictionary<int, string>()
+    {
+      {1, "one hundred"},{2 , "two hundred"},{3, "three hundred"},{4, "four hundred"},{5, "five hundred"},{6, "six hundred"},{7, "seven hundred"},{8, "eight hundred"},{9, "nine hundred"}
+    };
+    public static string ChangeToWords(string input)
     {
       List<string> outputStringList = new List<string>{};
       string outputString = "";
       char[] charArr = input.ToCharArray();
       for(int index = charArr.Length - 1; index >= 0; index--) 
       {
-        if (charArr.Length == 2 && charArr[0] == '1')
-        {
-          Console.WriteLine("First block");
-          outputString = (sub20[int.Parse(charArr[1].ToString())]);
-          break;
-        } 
-        else if (charArr.Length == 1)
+        if (charArr.Length == 1)
         {
           Console.WriteLine("Second block");
           outputString = sub10[int.Parse(charArr[0].ToString())];
         } 
+        else if (charArr.Length == 2 && charArr[0] == '1')
+        {
+          Console.WriteLine("First block");
+          outputString = (sub20[int.Parse(charArr[1].ToString())]);
+          break;
+        }
         else if (charArr.Length == 2 && charArr[0] != '1')
         {
           Console.WriteLine("Third block");
@@ -44,6 +48,60 @@ namespace NumbersToWords.Models
           outputString = string.Join("-", outputStringList);
           break;
         }
+        else if (charArr.Length == 3)
+        {
+          outputStringList.Add(sub1000[int.Parse(charArr[0].ToString())]);
+          if (charArr[1] == '1')
+          {
+            outputStringList.Add(sub20[int.Parse(charArr[1].ToString())]);
+          } 
+          else
+          {
+            List<string> twoDigitStringList = new List<string>{};
+            twoDigitStringList.Add(sub100[int.Parse(charArr[1].ToString())]);
+            twoDigitStringList.Add(sub10[int.Parse(charArr[0].ToString())]);
+            string twoDigitString = string.Join("-", twoDigitStringList);
+            outputStringList.Add(twoDigitString);
+            outputString = string.Join(" ", outputStringList);
+            break;
+          }
+        }
+        // else if (charArr.Length >= 4 && charArr.Length <= 6)
+        // {
+        //   if (charArr.Length == 4)
+        //   {
+        //   outputStringList.Add(sub10[int.Parse(charArr[0].ToString())]);
+        //   }
+        //   else if (charArr[4] == '1')
+        //   {
+        //     outputStringList.Add(sub20[int.Parse(charArr[1].ToString())]);
+        //   } 
+        //   else
+        //   {
+        //     List<string> twoThousandDigitStringList = new List<string>{};
+        //     twoThousandDigitStringList.Add(sub100[int.Parse(charArr[1].ToString())]);
+        //     twoThousandDigitStringList.Add(sub10[int.Parse(charArr[0].ToString())]);
+        //     string twoThousandDigitString = string.Join("-", twoThousandDigitStringList);
+        //     outputStringList.Add(twoThousandDigitString);
+        //     outputString = string.Join(" ", outputStringList);
+          
+        //     if (charArr[1] == '1')
+        //     {
+        //       outputStringList.Add(sub20[int.Parse(charArr[1].ToString())]);
+        //     } 
+        //     else
+        //     {
+        //       List<string> twoDigitStringList = new List<string>{};
+        //       twoDigitStringList.Add(sub100[int.Parse(charArr[1].ToString())]);
+        //       twoDigitStringList.Add(sub10[int.Parse(charArr[0].ToString())]);
+        //       string twoDigitString = string.Join("-", twoDigitStringList);
+        //       outputStringList.Add(twoDigitString);
+        //       outputString = string.Join(" ", outputStringList);
+        //       break;
+        //     }
+        //   }
+          
+        // }
       }
       
       return outputString;
